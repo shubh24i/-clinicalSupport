2631897e30ca77344845d7848252565350e802cc
sanjay <sanjay@smartkarrot.com>
2021-12-16 07:41:18 +0530

    css formating done

--- src/ClinicalSupport/ClinicalSupport.jsx
+++ src/ClinicalSupport/ClinicalSupport.jsx
@@ -2,35 +2,31 @@ import React, { useState } from "react";
 import './clinical.css';
 
 const ClinicalSupport = ({ children }) => {
-  const [fields, setFields] = useState([]);
 
+  const [fields, setFields] = useState([]);
   const handleChange = e => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = e => {
-      console.log("e.target.result", JSON.parse(e.target.result));
       setFields(JSON.parse(e.target.result)?.fields);
     };
   };
-
-  const orderByFields = fields?.sort((a,b)=>{
-    return a.order-b.order;
+  const orderByFields = fields?.sort((a, b) => {
+    return a.order - b.order;
   })
   return (
     <>
-      <h1>Upload Json file - Example</h1>
-
-      <input type="file" onChange={handleChange} />
-      <br />
+     <div className="uploadForm"> 
+        <h1>Upload Json file - Example</h1>
+        <input type="file" onChange={handleChange} />
+      </div>
+     
       <div className="form">
         <form>
-        <ul className="formWrap">
-          {orderByFields?.length>0 ? orderByFields?.map(item => {
-            return <FormItem {...item} key={item?.key} />
-          }):'No Data'}
-          {orderByFields?.length>0 && <li><span></span><button className="btn" type="submit">Submit</button></li>}
-
-        </ul>
+          <ul className="formWrap">
+            {orderByFields?.length > 0 ? orderByFields?.map(item => {return <FormItem {...item} key={item?.key} />}) : 'No Data'}
+            {orderByFields?.length > 0 && <li><span></span><button className="btn" type="submit">Submit</button></li>}
+          </ul>
         </form>
       </div>
     </>
@@ -41,11 +37,23 @@ export default ClinicalSupport
 
 
 
-const FormItem = ({ label, key, isRequired, order, isReadonly, type }) => {
+const FormItem = (props) => {
   return (
-    <li> <div className="col1">{label}</div>
-      <div className="col2"><input type={type} readOnly={isReadonly} required={isRequired} /></div></li>
+    <li> <div className="col1">{props?.label}</div>
+      <div className="col2">
+        {props?.type === 'dropdown' ? <Select {...props} /> : <Input {...props} />}
+      </div></li>
   );
+}
+
+const Input = ({ label, key, isRequired, isReadonly, type }) => {
+  return (<input type={type} name={label} readOnly={isReadonly} required={isRequired} />)
+}
+
+const Select = ({ label, items, key, isRequired, order, isReadonly, type }) => {
+  return (<select className={label} name={label} required={isRequired}>
+    {items?.map(({ value, text }) => <option key={`select${key}`} value={value}>{text}</option>)}
+  </select>)
 }
 
 
